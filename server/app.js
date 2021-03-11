3a62739e369ca83ef138652ca205dc617320189b
kemppi83 <kemppi83@gmail.com>
2021-03-11 09:20:09 +0100

    Express fetches gbg data, react fetches hello world from backend

--- server/app.js
+++ server/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
-const cors = require('cors');
 // const path = require('path');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   console.log('GET /');
-  res.json('Hellou World!');
+  const url = 'https://esb.goteborg.se/TEIK/Kalendarium/v1_0/activities';
+  const result = await fetch(url, {
+    method: 'get',
+    headers: { 'Authorization': 'Basic a2FsZW5kYXJpZWFwaTpWNVNcZVdzQA==' },
+  });
+  const data = await result.json();
+  console.log(data);
+  res.json(data);
+  // res.json('Hellou World!');
 });
 
 module.exports.app = app;
